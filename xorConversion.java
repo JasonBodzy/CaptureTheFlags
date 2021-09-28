fcd07ed0b5b2c075c0732bfe8541e44dea6c1d37
Jason Bodzy <jasonbodzy@gmail.com>
2021-09-28 13:34:30 -0400

    Add files via upload

---
+++ xorConversion.java
@@ -0,0 +1,31 @@
+public class xorConversion {
+
+
+    /*
+    Can you find the character I XOR'd this entire flag with.
+     Format: flag{.*}
+
+     69636e68747c3e61683e6a506d767b3c50773f7d72
+
+     input 69636e68747c3e61683e6a506d767b3c50773f7d72
+     input icnht|>ah>jPmv{<Pw?}r
+     */
+
+
+
+    public static void main(String[] args) {
+
+        String input = "icnht|>ah>jPmv{<Pw?}r";
+
+        for(int i = 0; i < 256; i++){
+            System.out.println("        " + i);
+            for(int j = 0; j < input.length(); j++){
+                System.out.print((char)(input.charAt(j) ^ i));
+            }
+
+
+        }
+
+
+    }
+}
